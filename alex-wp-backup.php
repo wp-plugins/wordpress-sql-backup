a8fe8211a4278daae9cee9087ade48a61f6f8ba5
Adzbierajewski <Adzbierajewski@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-27 18:24:43 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wordpress-sql-backup/trunk@429515 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- alex-wp-backup.php
+++ alex-wp-backup.php
@@ -3,7 +3,7 @@
 Plugin Name: WordPress SQL Backup
 Plugin URI: http://anthony.strangebutfunny.net/my-plugins/alex-wp-backup/
 Description: WordPress Backup allows you to create an sql backup of your WordPress database
-Version: 2.0
+Version: 3.0
 Author: Alex and Anthony
 Author URI: http://www.strangebutfunny.net/
 license: GPL 
