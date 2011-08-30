44df180d1077bba11ba3ab8b2e114c60ea157947
Adzbierajewski <Adzbierajewski@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-30 06:56:57 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wordpress-sql-backup/tags/4.0@430573 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ alex-wp-backup.php
@@ -0,0 +1,56 @@
+<?php
+/*
+Plugin Name: WordPress SQL Backup
+Plugin URI: http://anthony.strangebutfunny.net/my-plugins/alex-wp-backup/
+Description: WordPress Backup allows you to create an sql backup of your WordPress database
+Version: 4.0
+Author: Alex and Anthony
+Author URI: http://www.strangebutfunny.net/
+license: GPL 
+*/
+add_option( 'alex_backup_filename', 'nothing' );
+add_option( 'alex_backup_urlpath', 'nothing' );
+add_action('admin_notices', 'alex_backup_admin_notices');
+function alex_backup_admin_notices() {
+	$backup_options = get_option('dbmanager_options');
+	if(!@file_exists(WP_CONTENT_DIR . '/alex-backups/' . 'index.php')) {
+		echo '<div class="error" style="text-align: center;"><p style="color: red; font-size: 14px; font-weight: bold;">'.__('The folder /wp-content/alex-backups/ is visible to the public!', 'wp-postratings').'</p><p>'.sprintf(__('To fix this, create an empty file called <strong>index.php</strong> in the directory <strong>wp-content/alex-backups</strong>', 'wp-postratings')).'</p></div>';
+	}
+}
+
+	if(!@file_exists(WP_CONTENT_DIR . '/alex-backups/')) {
+mkdir(WP_CONTENT_DIR . "/alex-backups");
+	}
+	if(!@file_exists(WP_CONTENT_DIR . '/alex-backups/index.php')) {
+if (!copy(WP_CONTENT_DIR . "/plugins/wordpress-sql-backup/index.php", WP_CONTENT_DIR . "/alex-backups/index.php")) {
+    echo "failed to write file '" . WP_CONTENT_DIR . "/alex-backups/index.php" / "' in '" . WP_CONTENT_DIR . "/alex-backups/" . "'";
+}
+	}
+function alex_plugin_menu() {
+	add_menu_page('Backup Site', 'Backup Site', 'manage_options', 'alex_wp_backup', 'alex_plugin_options'); 
+}
+
+function alex_plugin_options() {
+	if (!current_user_can('manage_options'))  {
+		wp_die( __('You do not have sufficient permissions to access this page.') );
+	}
+	echo '<div class="wrap">';
+	echo "<h2><a class='submit' href=admin.php?page=alex_wp_backup&action=backup>Generate Backup</a></h2>";
+	if(isset($_REQUEST["action"])){
+	if($_REQUEST["action"]=="backup"){
+			require("sql_export.php");
+echo "What do you want to do?<br />";
+echo "<h3><a href='" . get_option('alex_backup_urlpath') . "'>Directly download the backup file</a></h3>";
+echo "<h3><a href='admin.php?page=alex_wp_backup&action=email_to_admin'>Email this backup to the administrator's email as an attachment</a></h3>";
+	}
+	if($_REQUEST["action"]=="email_to_admin"){
+	
+	wp_mail(get_option('admin_email'),'Alex Wordpress Backup', "Here's your backup","" ,get_option("alex_backup_filename"));
+	echo "There!, I sent the backup as an attachment to your email The attached backup file should look something like 4958e2917dbf36ea53b46c6b954917e9.sql Have fun!";
+	}
+	}
+	echo "<br /><p>Please visit my site <a href=http://www.strangebutfunny.net/>http://www.strangebutfunny.net</a></p>";
+	echo '</div>';
+}
+add_action('admin_menu', 'alex_plugin_menu');
+?>
---
+++ readme.txt
@@ -0,0 +1,45 @@
+=== Plugin Name ===
+Contributors: alexandanthony, Adzbierajewski
+Donate link: http://anthony.strangebutfunny.net/
+Tags: wordpress, backup
+Requires at least: 3.2.1
+Tested up to: 3.2.1
+Stable tag: 4.0
+
+WP Backup is a plugin that allows you to easily preform an sql backup from within your dashboard. The plugin is very secure and only allows administrators to preform a backup.
+ This plugin allows you to create a backup and download it or email it to the administrator as an attatchment. This plugin also works great and has been tested on blogs with thousands of posts.
+
+== Description ==
+
+WP Backup is a plugin that allows you to easily preform an sql backup from within your dashboard. The plugin is very secure and only allows administrators to preform a backup.
+ This plugin allows you to create a backup and download it or email it to the administrator as an attatchment. This plugin also works great and has been tested on blogs with thousands of posts.
+ 
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Upload `the directory "wordpress-sql-backup"` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Go to "Backup Site" in the dashboard to backup or download the backup or email the backup to yourself
+
+== Screenshots ==
+
+1. Here's a screenshot of it in action
+
+== Changelog ==
+
+= 1.0 =
+* Initial Release
+
+= 2.0 =
+* Wrong encoding bug fix
+
+= 3.0 =
+* Unable to restore backed up sql files bug fixed
+* Ability to download sql file to your computer
+* Ability to email the sql file to the administrator as an attatchment
+
+= 4.0 =
+* "index.php" in /alex-backups/ is now automatically created if possible
