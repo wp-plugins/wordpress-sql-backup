485158b4a391c18d55016d80a5b5e3f262c4ea30
Adzbierajewski <Adzbierajewski@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-11 09:34:22 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wordpress-sql-backup/tags/7.0@436252 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ sql_export.php
@@ -0,0 +1,22 @@
+<?php
+$filename_numbers = md5(rand(0,100) . rand(0,100) . rand(0,100) . rand(0,100) . rand(0,100) . rand(0,100) . rand(0,100) . rand(0,100) . rand(0,100) . rand(0,100));
+$filename = "/" . $filename_numbers . ".sql";
+$fh = fopen(get_option('alex_backup_filename'), 'w') or die("can't open file, are you sure I have permission to open the file?");
+fclose($fh);
+unlink(get_option('alex_backup_filename'));
+update_option('alex_backup_filename', WP_CONTENT_DIR . '/alex-backups/' . $filename);
+update_option('alex_backup_urlpath', get_bloginfo('wpurl') . '/wp-content' . '/alex-backups' . $filename);
+
+
+$backupfile = WP_CONTENT_DIR . '/alex-backups/' . $filename;
+	if(get_option("alex_backup_whattouse")=="exec"){
+exec("mysqldump --opt -h" . DB_HOST . " -u" . DB_USER . " -p" . DB_PASSWORD . " " . DB_NAME . " > " . $backupfile . "");
+		}
+			if(get_option("alex_backup_whattouse")=="passthru"){
+passthru("mysqldump --opt -h" . DB_HOST . " -u" . DB_USER . " -p" . DB_PASSWORD . " " . DB_NAME . " > " . $backupfile . "");
+		}
+			if(get_option("alex_backup_whattouse")=="system"){
+system("mysqldump --opt -h" . DB_HOST . " -u" . DB_USER . " -p" . DB_PASSWORD . " " . DB_NAME . " > " . $backupfile . "");
+		}
+echo "The backup was completed sucessfully!<br />";
+?>
